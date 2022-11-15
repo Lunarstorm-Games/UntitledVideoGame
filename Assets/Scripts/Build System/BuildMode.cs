593d83cc4e1d0f87d4ff9474a61a332b713b6e00
danielstegeman <daniel.stegeman@hotmail.com>
2022-11-15 14:21:29 +0100

    building

---
+++ Assets/Scripts/Build System/BuildMode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMode : MonoBehaviour
+{
+    public List<BuildableStructure> BuildableStructures;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Build System/BuildableStructure.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.SaveSystem;
+using UnityEngine;
+
+public class BuildableStructure : PersistableMonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
