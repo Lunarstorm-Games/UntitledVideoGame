e4db90636e0ec40dcec6777b22aa7058cd0f7bfd
RealSnowKid <realsnowkid@gmail.com>
2022-12-02 11:10:49 +0100

    Add Main Menu

--- Assets/Scripts/Menu UI/MainMenu.cs
+++ Assets/Scripts/Menu UI/MainMenu.cs
@@ -7,21 +7,36 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Texture2D cursorArrow;
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto); 
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
+    
     public void Start()
     {
         Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+    public void PlayGame()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        SceneManager.LoadScene("Main scene", LoadSceneMode.Single);
+        // Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.visible = false;
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("Options", LoadSceneMode.Single);
+    }
+
+    public void OpenDiscord()
+    {
+        Application.OpenURL("https://discord.com/");
+    }
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/LunarstormGames");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
