568ce3b4e68ca4c087ae2ab7f1da40ab0da065ae
Vincent-Lauwen <99258534+Vincent-Lauwen@users.noreply.github.com>
2022-12-02 13:31:40 +0100

    Defenses working. enemies target offset.

--- Assets/Scripts/Build System/BuildableStructure.cs
+++ Assets/Scripts/Build System/BuildableStructure.cs
@@ -5,21 +5,12 @@ using Assets.Scripts.Interfaces;
 using Assets.Scripts.SaveSystem;
 using UnityEngine;
 
-public class BuildableStructure : PersistableMonoBehaviour, IInteractable, IDamageable
+public class BuildableStructure : PersistableMonoBehaviour, IInteractable
 {
     public int EssenceCost = 0;
-    public float health = 300;
+
     public void Interact(GameObject source)
     {
-    }
-
-    // Update is called once per frame
-    protected void Update()
-    {
-        if (health < 0)
-        {
-            gameObject.SetActive(false);
-        }
     }
 
 
@@ -28,13 +19,4 @@ public class BuildableStructure : PersistableMonoBehaviour, IInteractable, IDama
         
     }
 
-    public void TakeDamage(float damage, Entity origin)
-    {
-        if (health > 0)
-        {
-
-        health-=damage;
-        }
-       
-    }
 }
