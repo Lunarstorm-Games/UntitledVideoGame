8a5ff7c5e38cc9b4576f864b387b08b3ccf44298
Daniel Stegeman <daniel.stegeman@hotmail.com>
2022-12-19 17:30:32 +0100

    added some particle effects

--- Assets/Scripts/Entities/TreeOfLife.cs
+++ Assets/Scripts/Entities/TreeOfLife.cs
@@ -7,19 +7,33 @@ public class TreeOfLife : Entity
 {
     [SerializeField] private HealthBarUI healthBar;
     [SerializeField] private UnityEvent onDeath;
+    public float hpPercentageForParticles = 0.1f;
+    public float startEffectDuration = 10f;
+    public GameObject DeathParticles;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = MaxHealth;
         healthBar.SetMaxHealth(MaxHealth);
+        // DeathParticles.SetActive(false);
+        StartCoroutine(StartEffect());
+    }
+
+    IEnumerator StartEffect()
+    {
+        yield return new WaitForSeconds(startEffectDuration);
+        DeathParticles.SetActive(false);
     }
 
     public override void TakeDamage(float damage, Entity origin)
     {
-
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+        if (currentHealth < maxHealth * hpPercentageForParticles && !DeathParticles.activeSelf)
+        {
+            DeathParticles.SetActive(true);
+        }
 
         if (currentHealth <= 0 && !Death)
         {
@@ -27,5 +41,4 @@ public class TreeOfLife : Entity
             onDeath?.Invoke();
         }
     }
-
 }
